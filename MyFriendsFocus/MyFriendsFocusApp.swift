ba19b70eab08069884dc02b5aa61316fd40d714c
German <lordireality@gmail.com>
2023-12-28 19:47:03 +0300

    Initial Commit

---
+++ MyFriendsFocus/MyFriendsFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFriendsFocusApp.swift
+//  MyFriendsFocus
+//
+//  Created by Герман Зыкин on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyFriendsFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
