222fd1ef1279c5a1c97246747ccfbcd371bb2e54
vladukha <vladic.p@gmail.com>
2023-12-29 23:58:20 +0300

    Table сделали на подписку хуйни

--- MyFriendsFocus/ContentView.swift
+++ MyFriendsFocus/ContentView.swift
@@ -13,18 +13,14 @@ struct ContentView: View {
     // StateObject нужен для хранения классов, за которыми View будет наблюдать и автоматически перерисовывать ui если наблюдаемые параметры изменились
     @StateObject private var viewModel = ContactManager()
     var body: some View {
-        var contactData = ContactManager.init()
-        if contactData.resp {
-            Table(contactData.contactData){
-                TableColumn("My Contacts"){ contact in
-                    HStack{
-                        contact.profilePicImg.resizable().frame(width: 50, height: 50).cornerRadius(15)
-                        Text(contact.fullName)
-                    }
+        // Сделали подписку на этот массив, теперь всегда при изменении этого параметра, Table будет перерисовываться
+        Table(viewModel.contactData){
+            TableColumn("My Contacts"){ contact in
+                HStack{
+                    contact.profilePicImg.resizable().frame(width: 50, height: 50).cornerRadius(15)
+                    Text(contact.fullName)
                 }
             }
-        } else {
-            Text(contactData.ex ?? "no exception but failed")
         }
     }
 }
