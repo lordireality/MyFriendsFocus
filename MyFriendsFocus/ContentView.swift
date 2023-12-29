e16ec9ec981cfbbc04984178e0ff96a66091e16a
vladukha <vladic.p@gmail.com>
2023-12-30 00:05:12 +0300

    пояснил за более крутое использование данных для картинки

--- MyFriendsFocus/ContentView.swift
+++ MyFriendsFocus/ContentView.swift
@@ -17,7 +17,17 @@ struct ContentView: View {
         Table(viewModel.contactData){
             TableColumn("My Contacts"){ contact in
                 HStack{
-                    contact.profilePicImg.resizable().frame(width: 50, height: 50).cornerRadius(15)
+                    // Здесь мы делаем цепочку развертываний, сначала проверяем есть ли дата, потом проверяем получилось ли создать картинку
+                    // UIImage - структура, которую можно с кайфом передавать, она хранит в себе данные о картинке и можно ее изменять кучей методов
+                    // Image - это чисто ui отображение картинки, не надо ее хранить нигде, лучше хранить UIImaage, Data, или ссылку на картинку
+                    if let data = contact.profilePicData,
+                    let image = UIImage(data: data){
+                        // модификаторы к View элементам принято с новой строки писать, это сильно читабельнее и пизды могут дать если в строку будешь ебашить
+                        Image(uiImage: image)
+                            .frame(width: 50, height: 50)
+                            .cornerRadius(15)
+                    }
+//                    contact.prof.resizable()
                     Text(contact.fullName)
                 }
             }
