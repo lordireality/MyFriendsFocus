a7046e23de9db94aa9de332946133f4904ec2a4b
German <lordireality@gmail.com>
2023-12-29 22:00:20 +0300

    created prototype of app

---
+++ MyFriendsFocus/ContactInfoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContactInfoViewModel.swift
+//  MyFriendsFocus
+//
+//  Created by Герман Зыкин on 29.12.2023.
+//
+
+import Foundation
---
+++ MyFriendsFocus/ContactManager.swift
@@ -0,0 +1,8 @@
+//
+//  ContactManager.swift
+//  MyFriendsFocus
+//
+//  Created by Герман Зыкин on 28.12.2023.
+//
+
+import Foundation
