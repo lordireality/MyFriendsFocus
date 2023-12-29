e08857469308280e836fce8f90575f034cf385a3
vladukha <vladic.p@gmail.com>
2023-12-30 00:14:07 +0300

    Вынес прикольно картинку в отдельную функцию для читаемости кода

--- MyFriendsFocus/ContentView.swift
+++ MyFriendsFocus/ContentView.swift
@@ -17,21 +17,28 @@ struct ContentView: View {
         Table(viewModel.contactData){
             TableColumn("My Contacts"){ contact in
                 HStack{
-                    // Здесь мы делаем цепочку развертываний, сначала проверяем есть ли дата, потом проверяем получилось ли создать картинку
-                    // UIImage - структура, которую можно с кайфом передавать, она хранит в себе данные о картинке и можно ее изменять кучей методов
-                    // Image - это чисто ui отображение картинки, не надо ее хранить нигде, лучше хранить UIImaage, Data, или ссылку на картинку
-                    if let data = contact.profilePicData,
-                    let image = UIImage(data: data){
-                        // модификаторы к View элементам принято с новой строки писать, это сильно читабельнее и пизды могут дать если в строку будешь ебашить
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFill() // Есть еще scaledToFit, но в данном случае так красивей
-                            .frame(width: 50, height: 50)
-                            .cornerRadius(15)
-                    }
-//                    contact.prof.resizable()
+                    contactImage(contact)
                     Text(contact.fullName)
                 }
+            }
+            
+        }
+    }
+    
+    private func contactImage(_ contact: ContactInfo) -> some View {
+        // оборачиваем в группу, чтобы при отсутствии картинки все равно был какой-то view для возврата функции
+        Group {
+            // Здесь мы делаем цепочку развертываний, сначала проверяем есть ли дата, потом проверяем получилось ли создать картинку
+            // UIImage - структура, которую можно с кайфом передавать, она хранит в себе данные о картинке и можно ее изменять кучей методов
+            // Image - это чисто ui отображение картинки, не надо ее хранить нигде, лучше хранить UIImaage, Data, или ссылку на картинку
+            if let data = contact.profilePicData,
+               let image = UIImage(data: data){
+                // модификаторы к View элементам принято с новой строки писать, это сильно читабельнее и пизды могут дать если в строку будешь ебашить
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill() // Есть еще scaledToFit, но в данном случае так красивей
+                    .frame(width: 50, height: 50)
+                    .cornerRadius(15)
             }
         }
     }
